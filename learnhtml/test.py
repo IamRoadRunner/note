af3c1a656c780ffc5e391b6c86a355d8c92c3506
wangze <aifirst@qq.com>
2017-12-28 17:48:04 +0800

    修改测试工具

--- learnhtml/test.py
+++ learnhtml/test.py
@@ -3,9 +3,8 @@ p={'tel':1,'personId':'001','agreement':True,'id':41}
 p2={'house':5787,'id':41,'value':11}
 i=0
 p3={'id':41}
-cookie=dict(cookies_sessionid='77xblj6ebwbn55qqcot97usp3e8h7886')
-for i in range(0,150):
+cookie=dict(sessionid='96rmn1wlkor8nqhf2iins4r3kd355f1k')
+for i in range(0,12):
 	#requests.post(" http://10.7.1.34/app/orderconfirm/",data=p3,cookies=cookie)
-	requests.get("http://10.7.1.34/app/eventinfo/?id=41")
-    
-    
+	#requests.get("http://10.7.1.34/app/detail/?id=41")
+	requests.get("http://10.7.1.34:5050/static/m/views/choiceHouse.html?id=41")
