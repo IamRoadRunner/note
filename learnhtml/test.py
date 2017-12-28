41880c1379476af2cd1c22d224766e43e36cfd4a
wangze <aifirst@qq.com>
2017-12-28 09:20:51 +0800

    requests测试接口

--- learnhtml/test.py
+++ learnhtml/test.py
@@ -1,9 +1,11 @@
 import requests
 p={'tel':1,'personId':'001','agreement':True,'id':41}
-p2={'house':5787,'id':41,'value':24}
+p2={'house':5787,'id':41,'value':11}
 i=0
-for i in range(0,15):
-	requests.post(" http://10.7.1.34/app/orderconfirm/",data=p2)
-	
+p3={'id':41}
+cookie=dict(cookies_sessionid='77xblj6ebwbn55qqcot97usp3e8h7886')
+for i in range(0,150):
+	#requests.post(" http://10.7.1.34/app/orderconfirm/",data=p3,cookies=cookie)
+	requests.get("http://10.7.1.34/app/eventinfo/?id=41")
     
     
