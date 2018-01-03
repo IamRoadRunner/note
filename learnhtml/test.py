d9c2e5039d45d51ab5f2991e7fc19c2b962d3bab
wangze <aifirst@qq.com>
2018-01-03 14:35:33 +0800

    requests

--- learnhtml/test.py
+++ learnhtml/test.py
@@ -1,10 +1,13 @@
 import requests
 p={'tel':1,'personId':'001','agreement':True,'id':41}
-p2={'house':5787,'id':41,'value':11}
+p2={'house':5792,'id':41,'value':11}
 i=0
 p3={'id':41}
-cookie=dict(sessionid='96rmn1wlkor8nqhf2iins4r3kd355f1k')
-for i in range(0,12):
-	#requests.post(" http://10.7.1.34/app/orderconfirm/",data=p3,cookies=cookie)
-	#requests.get("http://10.7.1.34/app/detail/?id=41")
-	requests.get("http://10.7.1.34:5050/static/m/views/choiceHouse.html?id=41")
+cookie=dict(sessionid='09doixacqg2m1a0rayyas4awccp5t4iu')
+for i in range(0,30):
+	#requests.post(" http://10.7.1.34:5050/app/orderconfirm/",data=p2,cookies=cookie)
+	#requests.get("http://10.7.1.34:5050/app/detail/",data=p3,cookies=cookie)
+	#requests.get("http://10.7.1.34:5050/static/m/views/choiceHouse.html?id=41")
+	#requests.get("http://10.7.1.34:5050/acc/cuslog/?tel=1&personId=001&agreement=true&id=41")
+	r=requests.get("http://10.7.1.34:5050/acc/cuslog/",data=p)
+	print(r.url)
