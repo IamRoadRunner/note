d0e9efb5c1493b3b95ca26536a50d4340ffb408a
wangze <aifirst@qq.com>
2018-01-05 17:33:07 +0800

    nginx 配置

--- learnhtml/test.py
+++ learnhtml/test.py
@@ -4,10 +4,10 @@ p2={'house':5792,'id':41,'value':11}
 i=0
 p3={'id':41}
 cookie=dict(sessionid='09doixacqg2m1a0rayyas4awccp5t4iu')
+cookie2=dict(token='ea566ddec685abf0a2caeb79d5883831',random='79421')
 for i in range(0,30):
 	#requests.post(" http://10.7.1.34:5050/app/orderconfirm/",data=p2,cookies=cookie)
 	#requests.get("http://10.7.1.34:5050/app/detail/",data=p3,cookies=cookie)
 	#requests.get("http://10.7.1.34:5050/static/m/views/choiceHouse.html?id=41")
 	#requests.get("http://10.7.1.34:5050/acc/cuslog/?tel=1&personId=001&agreement=true&id=41")
-	r=requests.get("http://10.7.1.34:5050/acc/cuslog/",data=p)
-	print(r.url)
+	r=requests.post("http://10.7.1.34:5050/acc/cuslog/",data=p,cookies=cookie2)
